65e99cb95b894b26a7042ee8e886ce7765e6055d
Matvei Stremous <matvei.stremous@gmail.com>
2025-06-08 13:23:01 +0300

    first commit

---
+++ src/main/java/com/marshall/sportbot/MarshallSportBotApplication.java
@@ -0,0 +1,11 @@
+package com.marshall.sportbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshallSportBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarshallSportBotApplication.class, args);
+    }
+}
