089a333002e42f9dc278bdb0477b07975ffc99ab
Matvei Stremous <matvei.stremous@gmail.com>
2025-06-15 01:54:22 +0300

    add empty tg bot

--- src/main/java/com/marshall/sportbot/MarshallSportBotApplication.java
+++ src/main/java/com/marshall/sportbot/MarshallSportBotApplication.java
@@ -1,11 +1,28 @@
 package com.marshall.sportbot;
 
+import com.marshall.sportbot.controller.SportBotController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
-public class MarshallSportBotApplication {
+@RequiredArgsConstructor
+public class MarshallSportBotApplication implements CommandLineRunner {
+
+    private final SportBotController botController;
+
     public static void main(String[] args) {
         SpringApplication.run(MarshallSportBotApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(botController);
+    }
 }
+
---
+++ src/main/java/com/marshall/sportbot/controller/SportBotController.java
@@ -0,0 +1,37 @@
+package com.marshall.sportbot.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class SportBotController extends TelegramLongPollingBot {
+
+
+    @Value("${bot.name}")
+    private String botUserName;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        log.info(String.valueOf(update));
+
+    }
+
+}
